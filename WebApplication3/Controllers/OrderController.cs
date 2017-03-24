6970271e29e0b3b1865727bd0a06756843b1f0ac
1103137241@gm.kuas.edu.tw <1103137241@gm.kuas.edu.tw>
2017-03-24 16:15:48 +0800

    3/24

---
+++ WebApplication3/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Controllers
+{
+    public class OrderController
+    {
+        //
+        //GET: /Order/
+
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrderById("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+    }
+}
