da63780afa88be53a1d7dceb353ff5ba20eca6de
1103137241@gm.kuas.edu.tw <1103137241@gm.kuas.edu.tw>
2017-03-24 16:04:45 +0800

    3/24

--- WebApplication3/Models/OrderService.cs
+++ WebApplication3/Models/OrderService.cs
@@ -5,24 +5,43 @@ using System.Web;
 
 namespace WebApplication3.Models
 {
+    /// <summary>
+    /// 訂單的服務
+    /// </summary>
     public class OrderService
     {
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
         public void InsertOrder()
         { 
-        }
 
+        }
+        /// <summary>
+        /// 刪除訂單
+        /// </summary>
         public void DeleteOrderById() 
         {
         
         }
-
+        /// <summary>
+        /// 更新訂單
+        /// </summary>
         public void UpdateOrder()
         { 
         
         }
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <param name="id">訂單ID</param>
+        /// <returns></returns>
         public Models.Order GetOrderById(string id)
         {
-            return new Models.Order();
+            Models.Order result = new Order();
+            result.CustId = "GSS";
+            result.CustName = "叡揚";
+            return result;
         }
 
         public List<Models.Order> GetOrders()
