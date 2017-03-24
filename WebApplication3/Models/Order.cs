93f856f99f7b13804a66a27d30fdc97206b61c34
1103137241@gm.kuas.edu.tw <1103137241@gm.kuas.edu.tw>
2017-03-24 15:57:09 +0800

    3/24

--- WebApplication3/Models/Order.cs
+++ WebApplication3/Models/Order.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace eSale.Models
+
+namespace WebApplication3.Models
 {
     public class Order
     {
---
+++ WebApplication3/Models/OrderService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class OrderService
+    {
+        public void InsertOrder()
+        { 
+        }
+
+        public void DeleteOrderById() 
+        {
+        
+        }
+
+        public void UpdateOrder()
+        { 
+        
+        }
+        public Models.Order GetOrderById(string id)
+        {
+            return new Models.Order();
+        }
+
+        public List<Models.Order> GetOrders()
+        {
+            return new List<Order>();
+        }
+    }
+}
